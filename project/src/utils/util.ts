6af784319a295c119fe7fac2d9c87014eff29186
Shamee <nachozzzz224@gmail.com>
2023-05-03 20:06:53 +0300

    Доработки: 1. Добавленны тесты редьюсеров. 2. Исправлен fetchUserStatus - ошибка в логике была выявленна Unit тестом. 3. Добавленна валидация пароля на странице Login

--- project/src/utils/util.ts
+++ project/src/utils/util.ts
@@ -1,19 +1,5 @@
-import { protoOffer } from '../types/types';
-import {CurrentOfferKey, DateFormant} from './consts';
+import { DateFormant} from './consts';
 import dayjs from 'dayjs';
-
-export const filterOffers = (offers: protoOffer[], currentKey: CurrentOfferKey, value: string | boolean | number) => {
-
-  const result: protoOffer[] = [];
-
-  for(const offer of offers ) {
-    if (offer[currentKey] === value) {
-      result.push(offer);
-    }
-  }
-
-  return result;
-};
 
 export const dateFormatting = (date: Date, dateFormat: DateFormant) => dayjs(date).format(dateFormat);
 
